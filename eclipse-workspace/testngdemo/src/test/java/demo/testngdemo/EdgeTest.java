e80e8a07769525c26923dc843e74c609bf46f56c
ManishaAgrawal3076 <manisha.agrawal.ext@capgemini.com>
2025-11-03 15:31:20 +0530

    Update EdgeTest.java

--- eclipse-workspace/testngdemo/src/test/java/demo/testngdemo/EdgeTest.java
+++ eclipse-workspace/testngdemo/src/test/java/demo/testngdemo/EdgeTest.java
@@ -20,21 +20,21 @@ public class EdgeTest {
 	@BeforeClass
 	public void setUp()
 	{
-		  System.setProperty("webdriver.edge.driver", "C:\\Users\\magrawa4\\Downloads\\edgedriver_win64\\msedgedriver.exe");
+		 /* System.setProperty("webdriver.edge.driver", "C:\\Users\\magrawa4\\Downloads\\edgedriver_win64\\msedgedriver.exe");
 	      driver = new EdgeDriver();
 	      driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-	      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));*/
 	}
 	
 	@Test(groups= {"A","B"})
 	public void loginTest()
 	{
-		driver.findElement(By.name("username")).sendKeys("Admin");
+	/*	driver.findElement(By.name("username")).sendKeys("Admin");
 		driver.findElement(By.name("password")).sendKeys("admin123");
 		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
 		
 		boolean status=driver.findElement(By.xpath("//h6[normalize-space()='Dashboard']")).isDisplayed();
-	    Assert.assertEquals(status, true);
+	    Assert.assertEquals(status, true);*/
 	
 	}
 
